45857d370fcccbdefa411834c6a4bef21ed375c0
Bytebandit-011 <reddyanush2004@gmail.com>
2026-05-31 16:59:43 +0530

    test commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from mlProject.pipeline.stage_02_data_validation import DataValidationPipeline
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline    
 
@@ -12,3 +13,12 @@ except Exception as e:
         raise e
  
 
+STAGE_NAME = "Data Validation Stage"
+try:
+      logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+      data_validation = DataValidationPipeline()
+      data_validation.main()
+      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
